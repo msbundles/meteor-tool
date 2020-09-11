cc073df80570af6e1ae92b88184522f139a81d7f
msbundles <25093511+msbundles@users.noreply.github.com>
2020-09-10 22:30:15 -0500

    I added the things to get lat and long, and parse the json. All I need to do now is present the data to the user.

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 let url = new URL("https://data.nasa.gov/resource/gh4g-9sfh.json?");
+//Var pointing to the form
 const form = document.querySelector('#form');
-let input = document.querySelector('input[name="radius"]');
+//Input from form
+var input = document.querySelector('input[name="radius"]');
 let request = new XMLHttpRequest();
+//Credit for these functions goes to http://www.movable-type.co.uk/scripts/latlong.html
+//I edited them a bit, but wanted to give credit
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+
+function getDistanceKm(let lat1,let lon1,let lat2,let lon2) {
+    let R = 6371;
+    let dLat = deg2rad(lat2-lat1);
+    let dLon = deg2rad(lon2-lon1);
+    let a =
+	Math.sin(dLat/2) * Math.sin(dLat/2) +
+	Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+	Math.sin(dLon/2) * Math.sin(dLon/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    let d = R * c;
+    return d;
+}
+
+//Function that runs when the geolocation fails
+function error() {
+    status.textContent = 'Unable to retrieve your location';
+}
+
 form.addEventListener('submit', event => {
-    event.preventDefault()
+    event.preventDefault();
     request.open('GET', url, true);
     request.onload = function () {
-
+	//If geolocation not supported tell the user
+	if(!navigator.geolocation) {
+	    alert('Geolocation is not supported by your browser');
+	} else {
+	    //If it is, get the lat and long and parse the json
+	    navigator.geolocation.getCurrentPosition(success => {
+		const ylatitude  = position.coords.latitude;
+		const ylongitude = position.coords.longitude;
+		let data = JSON.parse(request.response);
+		for (var i = 0; i < data.length; i++){
+		    var obj = data[i];
+		    if (getDistanceKm(ylatitude,ylongitude,obj.reclat,obj.reclong) <= input.value){
+			//Some way to get a list of meteors to the user.
+		    }
+		}
+	    }, error);
+	}
     }
     request.send();
-})
+});
 
